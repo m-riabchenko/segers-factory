b2fb586c4176eed46bebaeecf950b2e0c4522864
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-01-06 17:03:26 +0200

    added ViewSet for Category and Product

--- factory/catalog/views.py
+++ factory/catalog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from factory.catalog.models import Category, Product
+from factory.catalog.serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
