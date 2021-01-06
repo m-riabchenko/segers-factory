d72e745115d1b8d54166e8a9ca7d620b57910d72
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-01-06 17:02:39 +0200

    added product serializer

--- factory/catalog/serializers.py
+++ factory/catalog/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from factory.catalog.models import Category
+from factory.catalog.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
         fields = '__all__'
