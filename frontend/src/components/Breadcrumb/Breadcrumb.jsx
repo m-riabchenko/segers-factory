8dc445fe3552c80c8474ccc2d14e731df30764a8
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-02-12 22:33:10 +0200

    added use-react-router-breadcrumbs

--- frontend/src/components/Breadcrumb/Breadcrumb.jsx
+++ frontend/src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
+import {Link} from "react-router-dom";
 
-export const Breadcrumb = () => {
+export const Breadcrumb = ({namePage}) => {
+    const breadcrumbs = useBreadcrumbs();
     return (
-        <div className="ht__bradcaump__wrap">
+        <div className="ht__bradcaump__area">
+            <div className="ht__bradcaump__wrap">
                 <div className="container">
                     <div className="row">
                         <div className="col-xs-12">
                             <div className="bradcaump__inner text-center">
-                                <h2 className="bradcaump-title">Shop Page</h2>
+                                <h2 className="bradcaump-title">{namePage}</h2>
                                 <nav className="bradcaump-inner">
-                                    <a className="breadcrumb-item" href="index.html">Home</a>
-                                    <span className="brd-separetor">/</span>
-                                    <span className="breadcrumb-item active">Shop Page</span>
+                                    {breadcrumbs.map(({
+                                                          match,
+                                                          breadcrumb
+                                                      }) =>
+                                        <span key={match.url}><Link className="breadcrumb-item"
+                                                                    to={match.url}>{breadcrumb}<span
+                                            className="brd-separetor">/</span></Link></span>
+                                    )}
                                 </nav>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
