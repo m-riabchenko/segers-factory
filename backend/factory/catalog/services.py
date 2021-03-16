32ada02eae8a87ddb5bdd188f4c3c0e0c1a0e3f7
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-03-16 17:12:27 +0200

    backend; change response for product (added range price and options)

--- backend/factory/catalog/services.py
+++ backend/factory/catalog/services.py
@@ -1,40 +1,28 @@
+from collections import Counter
 from django.db.models import Q
-from django_filters import rest_framework as filters
+
 from factory.catalog.models import Product
 
 
-class ProductFilter(filters.FilterSet):
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
-    order_by = filters.CharFilter(method='order_by_product')
-    attr = filters.CharFilter(field_name="attributes", method='filter_product_attributes')
+def format_attr_params_to_dict(value) -> dict:
+    """
+    Format attributes string params to dict
+    """
+    params = [item for item in value.split("/") if item.strip()]
+    return dict(map(lambda x: x.split("="), params))
 
-    def filter_product_attributes(self, queryset, name, value):
-        """
-        Filter product by attributes which saved in json field
-        """
-        queries = Q()
-        query_params_list = [item for item in value.split("/") if item.strip()]
-        for key, value_ in dict(map(lambda x: x.split("="), query_params_list)).items():
-            query = Q()
-            for item in value_.split(","):
-                query |= Q(attributes__variant__contains=[{key: item}])
-            queries &= query
-        return queryset.filter(queries)
 
-    def order_by_product(self, queryset, name, value):
-        """
-        Filter order_by
-        """
-        if value == "low-price":
-            return queryset.order_by("price")
-        elif value == "high-price":
-            return queryset.order_by("-price")
-        return queryset
-
-    class Meta:
-        model = Product
-        fields = ['category', 'min_price', 'max_price']
+def get_queries_by_attr_params(params: dict):
+    """
+    Get queries for attributes field filter
+    """
+    queries = Q()
+    for key, value in params.items():
+        query = Q()
+        for item in value.split(","):
+            query |= Q(attributes__contains={key: item})
+        queries &= query
+    return queries
 
 
 def merge_dict(first_dict, second_dict) -> dict:
@@ -49,15 +37,71 @@ def merge_dict(first_dict, second_dict) -> dict:
     return first_dict
 
 
-def get_filters_data(category_id: int) -> dict:
+def get_filters_data(queryset) -> dict:
     """
     Get all variant attributes filter by category
     """
     data = {}
-    products = Product.objects.filter(category__id=category_id)
-    for product in products:
-        for item in product.attributes["variant"]:
-            data = merge_dict(data, item)
-    for key in data:
-        data[key] = list(set(data[key].split(",")))
+    for product in queryset:
+        data = merge_dict(data, product.attributes)
     return data
+
+
+def count_products_each_attr(queryset) -> dict:
+    """
+    Get quantity products each attributes by queryset
+    """
+    data = {}
+    for product in queryset:
+        data = merge_dict(data, product.attributes)
+    count_products = Counter()
+    for key in data:
+        count_products += Counter(data[key].split(','))
+    return count_products
+
+
+def get_chosen_list(formatted_attr):
+    chosen_list = []
+    for chosen_item in formatted_attr.values():
+        chosen_list.extend(chosen_item.split(','))
+    return chosen_list
+
+
+def get_options_for_product_filter(queryset, attr, category_id):
+    products_by_category = Product.objects.filter(category_id=category_id)
+    qty_products_in_category = count_products_each_attr(products_by_category)
+    qty_products_in_filter_queryset = count_products_each_attr(queryset)
+    all_products_attr_in_category = get_filters_data(products_by_category)
+    is_chosen = []
+
+    if attr is None:
+        products_quantity = qty_products_in_category
+    else:
+        formatted_attr = format_attr_params_to_dict(attr)
+        is_chosen = get_chosen_list(formatted_attr)
+        values = list(formatted_attr.keys())
+        if len(values) == 1:
+            # if in query params only one attribute is specified
+            qty_products_per_attr = Counter(all_products_attr_in_category[values[0]].split(','))
+            products_quantity = {**qty_products_in_filter_queryset, **qty_products_per_attr}
+        else:
+            products_quantity = qty_products_in_filter_queryset
+    return create_options(products_quantity, is_chosen, all_products_attr_in_category)
+
+
+def create_options(products_quantity: dict, is_chosen: list, all_attributes) -> list:
+    options_list = []
+    for key in all_attributes:
+        # set unique value
+        value = set(all_attributes[key].split(","))
+        options_list.append({
+            "option_name": key,
+            "option_value": [
+                {
+                    "option_value_name": attr_name,
+                    "is_chosen": True if attr_name in is_chosen else False,
+                    "products_quantity": products_quantity.get(attr_name, 0),
+                } for attr_name in value
+            ]
+        })
+    return options_list
--- backend/factory/catalog/views.py
+++ backend/factory/catalog/views.py
@@ -1,13 +1,15 @@
+from django.db.models import Max, Min, Count
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import MultiPartParser, JSONParser
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.filters import OrderingFilter
 from factory.catalog.models import Category, Product, Review, Rating
-from factory.catalog import serializers
-from factory.catalog.services import ProductFilter
+from factory.catalog import serializers, services
+from factory.catalog.serializers import ProductSerializer
+from django_filters import rest_framework as filters
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -21,13 +23,52 @@ class CategoryViewSet(viewsets.ModelViewSet):
                         status=status.HTTP_200_OK)
 
 
+class ProductFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+    attr = filters.CharFilter(field_name="attributes", method='filter_product_attr')
+
+    @staticmethod
+    def filter_product_attr(queryset, name, value):
+        """
+        Filter product by attributes which saved in json field
+        """
+        attr_params_dict = services.format_attr_params_to_dict(value)
+        queries = services.get_queries_by_attr_params(attr_params_dict)
+        return queryset.filter(queries)
+
+    class Meta:
+        model = Product
+        fields = ['category', 'min_price', 'max_price']
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
     parser_classes = (MultiPartParser, JSONParser)
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = ProductFilter
+    ordering_fields = ["price"]
+
+    def list(self, request):
+        if request.GET.get('attr') and not request.GET.get('category'):
+            return Response(
+                data={"message": "Chose category. Category params is required!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        products = self.filter_queryset(self.get_queryset())
+        serializer = ProductSerializer(products, many=True)
+        aggregate = products.aggregate(Max('price'), Min('price'), count=Count('id'))
+        return Response(data={
+            "options": services.get_options_for_product_filter(products, request.GET.get('attr'), request.GET.get('category')),
+            "quantity": aggregate['count'],
+            "products": serializer.data,
+            "range_price": {
+                "min": aggregate["price__min"],
+                "max": aggregate["price__max"]
+            }
+        })
 
     @action(methods=['put'], detail=True)
     def upload_image(self, request, pk=None):
