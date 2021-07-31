79dcdd5cfe928e137ab88982fd69a4fbeab499dd
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-07-31 17:38:06 +0300

    backend; update admin

--- backend/factory/catalog/admin.py
+++ backend/factory/catalog/admin.py
@@ -5,8 +5,6 @@ from mptt.admin import MPTTModelAdmin
 
 from factory.catalog.models import Category, Product, Review
 
-from factory.settings import HOST
-
 
 @admin.register(Review)
 class ReviewAdmin(MPTTModelAdmin):
@@ -14,20 +12,29 @@ class ReviewAdmin(MPTTModelAdmin):
     search_fields = ('full_name', "email")
 
 
-
 @admin.register(Category)
 class CategoryAdmin(MPTTModelAdmin):
-    list_display = ['name', 'date_created', 'date_updated']
-    search_fields = ('name', )
+    list_display = ['name', "parent", 'date_created', 'date_updated', "update_category"]
+    search_fields = ('name',)
+    change_list_template = "admin/change_list_category.html"
+    list_display_links = ('update_category',)
+    def has_add_permission(self, request):
+        return False
+
+    def update_category(self, obj):
+        return mark_safe(
+            f'<a href="/dashboard/category/{obj.pk}" target="_blank"><input type="button" value="оновити" /></a>')
+
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    fields = (('name', 'price'), 'description', 'category')
+    fields = ("category", ('name', "price"), "description", ("sale", "rating_avg"), "available",
+              "attributes", ('created', 'updated'))
     readonly_fields = ('created', 'updated')
     list_display = (
-        'name', 'price', 'category', 'rating_avg', 'sale', 'product_image', 'available', 'created',
-        'updated', 'update_product')
+        'id', 'name', 'price', 'category', 'rating_avg', 'sale', 'product_image', 'available',
+        'created', 'updated', 'update_product')
     list_editable = ('price', 'sale', 'available')
     list_filter = (
         ('category', RelatedDropdownFilter),
@@ -52,4 +59,4 @@ class ProductAdmin(admin.ModelAdmin):
 
     def update_product(self, obj):
         return mark_safe(
-            f'<a href="{HOST}dashboard/product-update/{obj.pk}" target="_blank"><input type="button" value="update" /></a>')
+            f'<a href="/dashboard/product-update/{obj.pk}" target="_blank"><input type="button" value="оновити" /></a>')
